86e15c077169e04af0e8a2f25a3b1e444f1e0ef9
Xolani Tobo <u23533057@up.ac.za>
2025-03-08 22:31:24 +0200

    Add Student Controller

---
+++ u23533057_Practical_02/Controllers/StudentController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using u23533057_Practical_02.Models;
+
+namespace u23533057_Practical_02.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult StudentsList()
+        {
+            List<StudentsList> students = new List<StudentsList>
+            {
+                new StudentsList()
+                {
+                    StudentNumber = "u23533057",
+                    FirstName = "Xolani",
+                    LastName = "Tobo",
+                    EmailAddress = "u23533057@tuks.co.za",
+                    myLink = "~/HTML/Xolani.html"
+                },
+
+                new StudentsList()
+                {
+                    StudentNumber = "u23552311",
+                    FirstName = "Unathi",
+                    LastName = "Mokgoetsi",
+                    EmailAddress = "u23552311@tuks.co.za",
+                    myLink = "~/HTML/Unathi.html"
+                },
+
+                new StudentsList()
+                {
+                    StudentNumber = "u22626752",
+                    FirstName = "Sihle",
+                    LastName = "Shange",
+                    EmailAddress = "u22626752@tuks.co.za",
+                    myLink = "~/HTML/Sihle.html"
+                },
+
+                new StudentsList()
+                {
+                    StudentNumber = "u23534134",
+                    FirstName = "Khensani",
+                    LastName = "Nkwinika",
+                    EmailAddress = "u23534134@tuks.co.za",
+                    myLink = "~/HTML/Khensani.html"
+                },
+
+                new StudentsList()
+                {
+                    StudentNumber = "u23583569",
+                    FirstName = "Makgoshi",
+                    LastName = "Mariri",
+                    EmailAddress = "u23583569@tuks.co.za",
+                    myLink = "~/HTML/Makgoshi.html"
+                },
+
+            };
+
+
+            return View(students);
+        }
+
+    }
+}
