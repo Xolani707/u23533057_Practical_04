3d8c7007d080aafa6a2e6351f846357306ad34e2
Xolani Tobo <u23533057@up.ac.za>
2025-03-08 15:06:33 +0200

    Add StudentList model

---
+++ u23533057_Practical_02/Models/StudentList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace u23533057_Practical_02.Models
+{
+	public class StudentList
+	{
+        [Display(Name = "Student Number")]
+        public string StudentNumber { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Email Address")]
+        public int EmailAddress { get; set; }
+
+        [Display(Name = "Link to personal page")]
+        public string myLink { get; set; }
+    }
+}
