61ba72ce3350b02450887d7310a8a81893e68358
Xolani Tobo <u23533057@up.ac.za>
2025-03-08 14:56:43 +0200

    Add project files.

---
+++ u23533057_Practical_02/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace u23533057_Practical_02
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ u23533057_Practical_02/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="u23533057_Practical_02.MvcApplication" Language="C#" %>
